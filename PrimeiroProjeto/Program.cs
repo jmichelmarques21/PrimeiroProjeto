d20b81802f121664d4555ffd519b4054b8b087d8
jmich <jmich@JEANMARQUES>
2023-04-18 09:01:03 -0300

    Adicionar arquivos de projeto.

---
+++ PrimeiroProjeto/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Globalization;
+
+namespace PrimeiroProjeto {
+    public class Program {
+        public static void Main(string[] args) {
+
+            Console.WriteLine("Entre com o seu nome completo: ");
+            string nome = Console.ReadLine();
+            Console.WriteLine("Quantos quartos tem na sua casa? ");
+            int quartos = int.Parse(Console.ReadLine());
+            Console.WriteLine("Entre com o preço de um produto: ", CultureInfo.InvariantCulture);
+            double preco = double.Parse(Console.ReadLine());
+            Console.WriteLine("Entre com o seu último nome, idade e altura: (mesma linha)", CultureInfo.InvariantCulture);
+            string[] vet = Console.ReadLine().Split(" ");
+            string lastName = vet[0];
+            int idade = int.Parse(vet[1]);
+            double altura = double.Parse(vet[2]);
+
+            Console.WriteLine();
+            Console.WriteLine();
+
+            Console.WriteLine(nome);
+            Console.WriteLine(quartos);
+            Console.WriteLine(preco.ToString("F2"), CultureInfo.InvariantCulture);
+            Console.WriteLine(lastName);
+            Console.WriteLine(idade);
+            Console.WriteLine(altura.ToString("F2"), CultureInfo.InvariantCulture);
+            
+
+
+
+        }
+
+    }
+}
